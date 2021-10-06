707146cc49e0ed22ac1d7b10ff812d039e16c507
jasmin nguyen <jasmin.nguyen@bulme.at>
2021-10-06 08:57:12 +0200

    Date edit

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+import datetime
+
 print("Guten Start ins neue Schuljahr!")
 
 name= input("Name eingeben:")
   
 print(f'Guten Tag, {name}!')
 
+x= datetime.datetime.now()
+print(x)
